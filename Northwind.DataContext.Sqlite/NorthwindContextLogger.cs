d8f76317e09496655f6530bfd06efaf2e04ad9ad
Devon Vendetti <v3nd3tti@icloud.com>
2025-08-26 21:41:03 -0700

    Add project files.

---
+++ Northwind.DataContext.Sqlite/NorthwindContextLogger.cs
@@ -0,0 +1,21 @@
+﻿using static System.Environment;
+
+namespace Northwind.EntityModels;
+
+public class NorthwindContextLogger
+{
+    public static void WriteLine(string message)
+    {
+        string folder = Path.Combine(GetFolderPath(SpecialFolder.LocalApplicationData), "ModernWeb", "efcore-logs");
+
+        if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+
+        string dateTimeStamp = DateTime.Now.ToString("yyyyMMdd_HHmm");
+
+        string path = Path.Combine(folder, $"northwindlog-{dateTimeStamp}.txt");
+
+        StreamWriter textFile = File.AppendText(path);
+        textFile.WriteLine(message);
+        textFile.Close();
+    }
+}
